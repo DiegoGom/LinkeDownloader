1be3df110ccbc102528071189ea1230edf5f1b61
Diego Gomez Gonzalez <7535794+DiegoGom@users.noreply.github.com>
2024-01-08 16:39:17 -0600

    Update download-video.js

--- js/download-video.js
+++ js/download-video.js
@@ -1,15 +1,10 @@
 /*By Lair_nula*/
 
-var id_player="vjs_video_3_html5_api";
-
 var ul_class_append="classroom-body__nav-actions";
 var id_download_button="dwn-vde"+getRandomInt(99999)+"";
 var subtitle_class="classroom-nav__subtitle";
 
-
-	if ($( ".vd-wnk2i" ).length ) {
-		$(".vd-wnk2i").remove();
-	}
+$(".vd-wnk2i").remove();
 
 setTimeout(function (){
 	 if(player_exist()==1){
@@ -24,14 +19,14 @@ setTimeout(function (){
 	 	});
 	 }
 
-},2000);
+},1000);
 
 
 
 
 /*Verify exist player*/
 function player_exist(){
-	let element = document.getElementById(id_player);
+	let element = $("video");
 	
 		if(typeof(element) != 'undefined' && element != null){
         	return 1;
@@ -42,8 +37,9 @@ function player_exist(){
 
 /*Add button and obtain data video*/
 function add_button(){
-	$(".vd-wnk2i").remove();
-	let url_video=$("#"+id_player).attr("src");
+
+	const videoElement = document.querySelector('video');
+	const url_video = videoElement.src;
 	let obtain_name_video=$("."+subtitle_class).text();
 	let name1 = encodeURIComponent(obtain_name_video);
 	let name2 = name1.replace(/%0A/g, "");
